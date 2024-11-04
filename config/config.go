5d2044044f70b26af0af140c56e7d2ff7f4d2052
daria_smyr <dasha.smyr@gmail.com>
2024-11-05 00:26:29 +0300

    Added config

---
+++ config/config.go
@@ -0,0 +1,64 @@
+package config
+
+import (
+	"flag"
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"development"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string `yaml:"address" env-default:"0.0.0.0:8080"`
+	Timeout     string `yaml:"timeout" env-default:"5s"`
+	IdleTimeout string `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := fetchConfigPath()
+	if configPath == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(configPath); err != nil {
+		log.Fatalf("error loading config file %s", err)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("error loading config file %s", err)
+	}
+
+	return &cfg
+}
+
+// fetchConfigPath fetches domain path from command line flag or environment variable.
+// Priority: flag > env > default.
+// Default value is empty string.
+func fetchConfigPath() string {
+	var res string
+
+	cwd, _ := os.Getwd()
+	fmt.Println("Current working directory:", cwd)
+
+	if !flag.Parsed() { // Check if flag has been parsed
+		flag.StringVar(&res, "config", "", "path to config file")
+	}
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	fmt.Println("Config path:", res)
+
+	return res
+}
