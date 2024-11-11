470f1718b98a66ce87af2f504d7d67c7014181ae
daria_smyr <dasha.smyr@gmail.com>
2024-11-11 19:52:03 +0300

    Moved extractBearerToken to separate utils.go file.

---
+++ cmd/passvault/middleware/auth/utils.go
@@ -0,0 +1,16 @@
+package authrest
+
+import (
+	"net/http"
+	"strings"
+)
+
+func extractBearerToken(r *http.Request) string {
+	authHeader := r.Header.Get("Authorization")
+	splitToken := strings.Split(authHeader, "Bearer ")
+	if len(splitToken) != 2 {
+		return ""
+	}
+
+	return splitToken[1]
+}
