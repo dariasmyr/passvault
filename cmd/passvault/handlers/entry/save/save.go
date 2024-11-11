2d311c842aca1ea0e2ef693fac15758318433507
daria_smyr <dasha.smyr@gmail.com>
2024-11-11 20:37:28 +0300

    Moved response data to a separate struct

--- cmd/passvault/handlers/entry/save/save.go
+++ cmd/passvault/handlers/entry/save/save.go
@@ -1,12 +1,13 @@
 package save
 
+import resp "passvault/internal/lib/api/response"
+
 type Request struct {
 	EntryType string `json:"entry_type" validate:"required"`
 	EntryData string `json:"entry_data" validate:"required"`
 }
 
 type Response struct {
-	Status string `json:"status"`
-	Error  string `json:"error,omitempty"`
-	ID     int64  `json:"id"`
+	resp.Response
+	ID int64 `json:"id"`
 }
