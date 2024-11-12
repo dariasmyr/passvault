18daea4a936bf38f34be42a1c5fc1894247555b9
smyr <dasha.smyr@gmail.com>
2024-11-12 17:14:39 +0300

    Added constructor for SaveEntry handler

--- internal/http-server/handlers/entry/save/save.go
+++ internal/http-server/handlers/entry/save/save.go
@@ -2,7 +2,15 @@ package save
 
 import (
 	"context"
+	"errors"
+	"io"
+	"log/slog"
+	"net/http"
 	resp "passvault/internal/lib/api/response"
+	"passvault/internal/lib/logger/sl"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 )
 
 type Request struct {
@@ -17,4 +25,44 @@ type Response struct {
 
 type EntrySaver interface {
 	SaveEntry(ctx context.Context, entryType, entryData string) (int64, error)
+}
+
+func New(log *slog.Logger, entrySaver EntrySaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		const op = "handlers.entry.save.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+
+		if errors.Is(err, io.EOF) {
+			log.Error("request body is empty")
+
+			render.JSON(w, r, resp.Response{
+				Status: resp.StatusError,
+				Error:  "empty request",
+			})
+
+			return
+		}
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+
+			render.JSON(w, r, resp.Response{
+				Status: resp.StatusError,
+				Error:  "failed to decode request",
+			})
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("req", req))
+
+	}
 }
