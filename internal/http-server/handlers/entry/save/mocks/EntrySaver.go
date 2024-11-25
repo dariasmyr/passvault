161e7e1a901612d59120c2a1b8d3ee57fca12a98
daria_smyr <dasha.smyr@gmail.com>
2024-11-25 17:30:49 +0300

    Added mockConstructorTestingTEntrySaver and NewEntrySaver for mock EntrySaver

--- internal/http-server/handlers/entry/save/mocks/EntrySaver.go
+++ internal/http-server/handlers/entry/save/mocks/EntrySaver.go
@@ -12,3 +12,17 @@ func (m *MockEntrySaver) SaveEntry(accountId int64, entryType, entryData string)
 	args := m.Called(accountId, entryType, entryData)
 	return args.Get(0).(int64), args.Error(1)
 }
+
+type mockConstructorTestingTEntrySaver interface {
+	mock.TestingT
+	Cleanup(func())
+}
+
+func NewEntrySaver(t mockConstructorTestingTEntrySaver) *MockEntrySaver {
+	mock := &MockEntrySaver{}
+	mock.Mock.Test(t)
+
+	t.Cleanup(func() { mock.AssertExpectations(t) })
+
+	return mock
+}
