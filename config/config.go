7a52ab4044534dbd79c78e6ce6959432a4b9335c
daria_smyr <dasha.smyr@gmail.com>
2024-11-26 16:49:44 +0300

    Changed IdleTimeout type from HTTPServer to time.Duration

--- config/config.go
+++ config/config.go
@@ -18,7 +18,7 @@ type Config struct {
 type HTTPServer struct {
 	Address     string        `yaml:"address" env-default:"0.0.0.0:8080"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
-	IdleTimeout string        `yaml:"idle_timeout" env-default:"60s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
 
 func MustLoad() *Config {
